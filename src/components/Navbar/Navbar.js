e57bb2b9413c5c455dc6aa13d6388b19ab79249d
suportdevs <suportdevs@gmail.com>
2022-11-29 21:39:19 +0600

    modify menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,9 @@ const Navbar = () => {
     return (
         <div className='absolute left-0 top-0 right-0 z-10 container mx-auto'>
             <div className="grid grid-cols-2">
-                <div><h2 className='p-5'>Doctor Portal</h2></div>
+                <div>
+                    <h2 className='p-5'>Doctor Portal</h2>
+                </div>
                 <div className='flex justify-self-end items-center'>
                     <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300'>Home</a>
                     <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300'>About</a>
