ee187bdfb0505fd0d7404df4afa7fe1bab5aa1d8
suportdevs <suportdevs@gmail.com>
2022-12-14 22:40:43 +0600

    added mobile menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = useState();
+    const [toggleMenu, setToggleMenu] = useState('hidden');
     return (
         <div className='absolute left-0 top-0 right-0 z-10 container mx-auto'>
             <div className="grid grid-cols-2">
@@ -10,6 +10,18 @@ const Navbar = () => {
                     <h2 className='py-5 font-bold'>Doctor Portal</h2>
                 </div>
                 <div className='flex justify-self-end items-center'>
+                    <Bars3Icon onClick={(e) => setToggleMenu('block')} className={`${(toggleMenu === 'hidden') ? 'block' : 'hidden'} w-6 lg:hidden`}/>
+                    <XMarkIcon onClick={(e) => setToggleMenu('hidden')} className={`${toggleMenu} w-6 lg:hidden`}/>
+                    <div className={`${toggleMenu} absolute w-full left-0 top-16 shadow bg-white lg:block lg:relative lg:top-0 lg:shadow-none lg:transparent`}>
+                        <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300 block lg:inline-block'>Home</a>
+                        <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300 block lg:inline-block'>About</a>
+                        <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300 block lg:inline-block'>Appointment</a>
+                        <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300 block lg:inline-block'>Reviews</a>
+                        <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300 block lg:inline-block'>Contact Us</a>
+                        <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300 block lg:inline-block'>Login</a>
+                    </div>
+                </div>
+                {/* <div className='flex justify-self-end items-center'>
                     <Bars3Icon className='w-6 lg:hidden'/>
                     <div className='hidden lg:block'>
                         <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300'>Home</a>
@@ -19,7 +31,7 @@ const Navbar = () => {
                         <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300'>Contact Us</a>
                         <a className='p-3 cursor-pointer hover:bg-gray-700 hover:rounded hover:text-gray-300'>Login</a>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
